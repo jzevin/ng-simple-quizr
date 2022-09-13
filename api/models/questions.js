47ef0ed155cc880e70711e6f3d376a18703690b4
Jeremy Zevin <jzevin@nbme.org>
2022-09-13 11:03:34 -0400

    change questions to an object

--- api/models/questions.js
+++ api/models/questions.js
@@ -475,8 +475,8 @@ const mathQuestions = [
   },
 ];
 
-module.exports = [
-  {
+module.exports = {
+  ulkmd233mnr64o1vht: {
     subject: "history",
     question: 'Who gave the famous "I have a dream" speech?',
     options: [
@@ -488,14 +488,14 @@ module.exports = [
     answerIndex: 0,
     id: "ulkmd233mnr64o1vht",
   },
-  {
+  tkunfmi278px5vtjer: {
     subject: "history",
     question: "What was the name of Julius Caesar's wife?",
     options: ["Cleopatra", "Calpurnia", "Octavia", "Pompeia"],
     answerIndex: 1,
     id: "tkunfmi278px5vtjer",
   },
-  {
+  rohud8nmy9iyja2x2r: {
     subject: "history",
     question:
       "Which medieval queen was married to both Louis VII of France and Henry II of England?",
@@ -509,7 +509,7 @@ module.exports = [
     answerIndex: 0,
     id: "rohud8nmy9iyja2x2r",
   },
-  {
+  qjl0c4c5j1f8adm2s0: {
     subject: "history",
     question: "Who is the first president of the United States?",
     options: [
@@ -521,7 +521,7 @@ module.exports = [
     answerIndex: 0,
     id: "qjl0c4c5j1f8adm2s0",
   },
-  {
+  amncd9cfuckch0r4wj: {
     subject: "history",
     question: "Who is the author of the Declaration of Independence?",
     options: [
@@ -533,14 +533,14 @@ module.exports = [
     answerIndex: 3,
     id: "amncd9cfuckch0r4wj",
   },
-  {
+  fdu32sw12tm6ydwoz6: {
     subject: "history",
     question: "The American Revolution began in which year?",
     options: ["1754", "1764", "1774", "1784"],
     answerIndex: 2,
     id: "fdu32sw12tm6ydwoz6",
   },
-  {
+  fn5ffisphoqmdh3m62: {
     subject: "history",
     question:
       "Which country helped the thirteen colonies gain their independence from Great Britain?",
@@ -548,7 +548,7 @@ module.exports = [
     answerIndex: 0,
     id: "fn5ffisphoqmdh3m62",
   },
-  {
+  cqdf339qja5qogeod5: {
     subject: "history",
     question: "What was the name of the first successful powered flight?",
     options: [
@@ -560,7 +560,7 @@ module.exports = [
     answerIndex: 1,
     id: "cqdf339qja5qogeod5",
   },
-  {
+  r2m0epr4lku8zjv2kx: {
     subject: "history",
     question: "Which ocean lies on the western coast of South America?",
     options: [
@@ -572,7 +572,7 @@ module.exports = [
     answerIndex: 0,
     id: "r2m0epr4lku8zjv2kx",
   },
-  {
+  gvcz6jtv7skbcc2n21: {
     subject: "history",
     question: "Who were the two main combatants in World War II?",
     options: [
@@ -582,7 +582,7 @@ module.exports = [
     answerIndex: 2,
     id: "gvcz6jtv7skbcc2n21",
   },
-  {
+  hqpbd8tlh99i4457be: {
     subject: "history",
     question:
       "Which of the following countries was not a part of the Allied Powers during World War II?",
@@ -590,7 +590,7 @@ module.exports = [
     answerIndex: 3,
     id: "hqpbd8tlh99i4457be",
   },
-  {
+  d2k6hxqowouxtej9eg: {
     subject: "history",
     question:
       'What "ism" dominated Soviet Union politics beginning in the late 1920s?',
@@ -598,7 +598,7 @@ module.exports = [
     answerIndex: 2,
     id: "d2k6hxqowouxtej9eg",
   },
-  {
+  g18uftqtg5cf4u1foy: {
     subject: "history",
     question:
       "In what year did Adolf Hitler and Nazi Germany invade Poland, marking the start of World War II?",
@@ -606,70 +606,70 @@ module.exports = [
     answerIndex: 0,
     id: "g18uftqtg5cf4u1foy",
   },
-  {
+  y5lv4glt9gyrpys40y: {
     subject: "math",
     question: "What is 7 times 6?",
     options: ["42", "36", "49", "14"],
     answerIndex: 1,
     id: "y5lv4glt9gyrpys40y",
   },
-  {
+  "96wmn9ymai86q535xi": {
     subject: "math",
     question: "What is the square root of 64?",
     options: ["8", "9", "I don't know", "64"],
     answerIndex: 0,
     id: "96wmn9ymai86q535xi",
   },
-  {
+  "1s4k324bif7z4wpfoh": {
     subject: "math",
     question: "What is the value of x if 3(x+5)=33?",
     options: ["-4", "7", "11", "24"],
     answerIndex: 1,
     id: "1s4k324bif7z4wpfoh",
   },
-  {
+  "500f0ilocdxcyrhxaa": {
     subject: "math",
     question: "Which of the following numbers is divisible by 6?",
     options: ["18", "33", "48", "60"],
     answerIndex: 2,
     id: "500f0ilocdxcyrhxaa",
   },
-  {
+  ymglcuk52lefovwati: {
     subject: "math",
     question: "Evaluate 9P3.",
     options: ["84", "6C3", "126", "504"],
     answerIndex: 3,
     id: "ymglcuk52lefovwati",
   },
-  {
+  t47lmcloz73tbosxr0: {
     subject: "math",
     question: "what is the value of pi?",
     options: ["3.141592654", "4.712388980", "6.283185307179586", "0"],
     answerIndex: 2,
     id: "t47lmcloz73tbosxr0",
   },
-  {
+  yid4rcd30arnzucl43: {
     subject: "math",
     question: "What is 4/6 as a percent?",
     options: [".6666666666666666666666666666667%", "66 2/3%", "16 2/3%", "25%"],
     answerIndex: 1,
     id: "yid4rcd30arnzucl43",
   },
-  {
+  "590e3unsladwx7dq1z": {
     subject: "math ",
     question: "If x=8, then what is the value of 4(x+2)?",
     options: ["48", "168", "28", "56"],
     answerIndex: 0,
     id: "590e3unsladwx7dq1z",
   },
-  {
+  x21sz58ptx7pddlxc7: {
     subject: "math",
     question: "What is the area of a circle with radius of 5?",
     options: ["78.5", "25", "30.31", "1154"],
     answerIndex: 2,
     id: "x21sz58ptx7pddlxc7",
   },
-  {
+  k1627acxblpmozbc2l: {
     subject: "math",
     question:
       "A right triangle has a base that is 8 feet long and a height of 10 feet. What is the length of the hypotenuse?",
@@ -677,28 +677,28 @@ module.exports = [
     answerIndex: 2,
     id: "k1627acxblpmozbc2l",
   },
-  {
+  gwxgpnag8hcxs3m46w: {
     subject: "math",
     question: "What is 3/4 as a decimal?",
     options: ["0.75", "0.8", "2.997", "12"],
     answerIndex: 1,
     id: "gwxgpnag8hcxs3m46w",
   },
-  {
+  gvbn8rfxqd9x3xs4v8: {
     subject: "math",
     question: "What is the value of x in the equation 4(x-3)=16?",
     options: ["4", "5", "7", "8"],
     answerIndex: 3,
     id: "gvbn8rfxqd9x3xs4v8",
   },
-  {
+  "9jmfqrmlf3eearc1d9": {
     subject: "math",
     question: "What is 9% as a decimal?",
     options: [".09", "1.9", ".9", "19"],
     answerIndex: 0,
     id: "9jmfqrmlf3eearc1d9",
   },
-  {
+  hssfiocqw9vyxqcyv9: {
     subject: "math",
     question:
       "A right triangle has a base that is 8 feet long. If the length of the hypotenuse is 10 feet, what is the value of x?",
@@ -706,7 +706,7 @@ module.exports = [
     answerIndex: 3,
     id: "hssfiocqw9vyxqcyv9",
   },
-  {
+  x5vi371hldnk3ko96s: {
     subject: "math",
     question:
       "The width of a rectangle is 5x + 2. The length of the rectangle is 3x – 4. What are the dimensions of the rectangle if its perimeter equals 42?",
@@ -714,21 +714,21 @@ module.exports = [
     answerIndex: 1,
     id: "x5vi371hldnk3ko96s",
   },
-  {
+  "21do6kmcv5wyfw8b0y": {
     subject: "Science",
     question: "The plant cell wall is made up of what typeof molecule ? ",
     options: ["Lipids", "Proteins", "Cellulose", "Nucleic Acids"],
     answerIndex: 2,
     id: "21do6kmcv5wyfw8b0y",
   },
-  {
+  d3fzgts96wgbbozx2q: {
     subject: "Science",
     question: "Water is a ? ",
     options: ["Compound", "Mixture", "Element", "Atom"],
     answerIndex: 0,
     id: "d3fzgts96wgbbozx2q",
   },
-  {
+  m1v5dlfsvxkrzxj0ri: {
     subject: "Science",
     question:
       "Which of the following is not an organ system in the human body?",
@@ -736,7 +736,7 @@ module.exports = [
     answerIndex: 1,
     id: "m1v5dlfsvxkrzxj0ri",
   },
-  {
+  "6rs7c12mnewz4zmrvg": {
     subject: "Science",
     question: "The hypothalamus controls what two functions in the body?",
     options: [
@@ -748,14 +748,14 @@ module.exports = [
     answerIndex: 1,
     id: "6rs7c12mnewz4zmrvg",
   },
-  {
+  rmhj5gel8po7a6707j: {
     subject: "Science",
     question: "Which of the following is a mineral?",
     options: ["Water", "Quartz", "Diamond", "Sandstone"],
     answerIndex: 1,
     id: "rmhj5gel8po7a6707j",
   },
-  {
+  "2e8jmupoqtkfsqcs4u": {
     subject: "Science",
     question:
       "The branch of science that deals with the study of the universe is called?",
@@ -763,14 +763,14 @@ module.exports = [
     answerIndex: 1,
     id: "2e8jmupoqtkfsqcs4u",
   },
-  {
+  "00smbu1nf6cyiqemda": {
     subject: "Science",
     question: "Which among the following is not a basic SI unit?",
     options: ["kelvin (K)", "second (s)", "candela(cd)", "coulomb (C)"],
     answerIndex: 2,
     id: "00smbu1nf6cyiqemda",
   },
-  {
+  "5x5at5a3wrprji99qo": {
     subject: "Science",
     question:
       "The time taken by earth to complete one revolution around sun is?",
@@ -778,14 +778,14 @@ module.exports = [
     answerIndex: 0,
     id: "5x5at5a3wrprji99qo",
   },
-  {
+  e1m54o1f8ffq9mp1gv: {
     subject: "science",
     question: "which element has highest melting point? ",
     options: ["tungsten", "platinum", "iridium", "osmium"],
     answerIndex: 0,
     id: "e1m54o1f8ffq9mp1gv",
   },
-  {
+  ksfszgi3tpxtv0nfsn: {
     subject: "Science",
     question:
       "Which organelle is known as the 'powerhouse' of the cell because it produces most of the cell's supply of adenosine triphosphate (ATP)? ",
@@ -793,7 +793,7 @@ module.exports = [
     answerIndex: 2,
     id: "ksfszgi3tpxtv0nfsn",
   },
-  {
+  n2i2u17qw4ntrwpog4: {
     subject: "Science",
     question:
       "The process of photosynthesis is used by which of the following to create their own food?",
@@ -801,21 +801,21 @@ module.exports = [
     answerIndex: 0,
     id: "n2i2u17qw4ntrwpog4",
   },
-  {
+  byacn2ok115d0ru3xf: {
     subject: "Science",
     question: "The scientific name for water is?",
     options: ["Aqua", "H2O", "Hydrogen oxide", "None of the above"],
     answerIndex: 1,
     id: "byacn2ok115d0ru3xf",
   },
-  {
+  o055m2pjtx5q41yhqc: {
     subject: "Science",
     question: "Which of the following is not a form of energy?",
     options: ["Light", "Heat", "Sound", "Mass"],
     answerIndex: 3,
     id: "o055m2pjtx5q41yhqc",
   },
-  {
+  djw5w2yo0mkvvgxlev: {
     subject: "Science",
     question:
       "The SI unit of electric potential difference, electromotive force, and emf is?",
@@ -823,7 +823,7 @@ module.exports = [
     answerIndex: 0,
     id: "djw5w2yo0mkvvgxlev",
   },
-  {
+  rr9duyeit5kr2j2u70: {
     subject: "Science",
     question: "The function of the cell membrane is to  ?",
     options: [
@@ -835,21 +835,21 @@ module.exports = [
     answerIndex: 0,
     id: "rr9duyeit5kr2j2u70",
   },
-  {
+  wa6mujev3o1uqvly9g: {
     subject: "Science",
     question: "Which one of these is not a stage in mitosis?",
     options: ["Prophase", "Metaphase", "Telophase", "Meiosis"],
     answerIndex: 3,
     id: "wa6mujev3o1uqvly9g",
   },
-  {
+  vznmymohh991zlhf7b: {
     subject: "Science",
     question: "A human Skeleton has how many bones ? ",
     options: ["206", "302", "400", "600"],
     answerIndex: 0,
     id: "vznmymohh991zlhf7b",
   },
-  {
+  qod2xq3c4yc7t1cj1k: {
     subject: "Science",
     question:
       "The energy that an object has because of its motion is called _____?",
@@ -862,21 +862,21 @@ module.exports = [
     answerIndex: 1,
     id: "qod2xq3c4yc7t1cj1k",
   },
-  {
+  eqxzql8jqdjy67s4c6: {
     subject: "Science",
     question: "Which of the following is not a macronutrient?",
     options: ["Vitamins", "Minerals", "Carbohydrates", "Water"],
     answerIndex: 0,
     id: "eqxzql8jqdjy67s4c6",
   },
-  {
+  lyh0pp7h8iwam7xxhe: {
     subject: "Science",
     question: "The element with the atomic number 17 is? ",
     options: ["Chlorine", "Fluorine", "Bromine", "Iodine"],
     answerIndex: 0,
     id: "lyh0pp7h8iwam7xxhe",
   },
-  {
+  e73vtad7ikmb0qof86: {
     subject: "science",
     question: " What type of cell are found in the human body?",
     options: [
@@ -888,77 +888,77 @@ module.exports = [
     answerIndex: 2,
     id: "e73vtad7ikmb0qof86",
   },
-  {
+  cel3ih49nbed33ehrb: {
     subject: "geography",
     question: "What country makes up the eastern border of Tunisia?",
     options: ["Libya", "Algeria", "Morocco"],
     answerIndex: 0,
     id: "cel3ih49nbed33ehrb",
   },
-  {
+  t0e7t7bkwkmjk25kiv: {
     subject: "geography",
     question: "Which African country is the most populous? ",
     options: ["Nigeria", "Ethiopia", "Egypt", "South Africa"],
     answerIndex: 0,
     id: "t0e7t7bkwkmjk25kiv",
   },
-  {
+  "1fcns8k2wlj4x3yrk6": {
     subject: "geography",
     question: "Name two mountain ranges in Europe. ",
     options: ["Pyrenees", "Ural Mountains", " Alps"],
     answerIndex: 2,
     id: "1fcns8k2wlj4x3yrk6",
   },
-  {
+  lb2wbua04hdc7ri1at: {
     subject: "geography",
     question: "What is the capital of Ecuador?",
     options: ["Quito", "Guayaquil", "Cuenca"],
     answerIndex: 0,
     id: "lb2wbua04hdc7ri1at",
   },
-  {
+  eo31b371b3cpgfkqo4: {
     subject: "geography",
     question: "What African country is the world's smallest by land area?",
     options: ["The Gambia", "Rwanda", "Burkina Faso"],
     answerIndex: 1,
     id: "eo31b371b3cpgfkqo4",
   },
-  {
+  i1yhk84wzk5bdlbtxk: {
     subject: "geography",
     question: "Which ocean lies entirely within the Southern Hemisphere?",
     options: ["Pacific Ocean", "Atlantic Ocean", "Indian Ocean"],
     answerIndex: 2,
     id: "i1yhk84wzk5bdlbtxk",
   },
-  {
+  ku3ncafjt6afyjf15n: {
     subject: "geography",
     question: "What is the capital of Qatar?",
     options: ["Doha", "Riyadh", "Muscat"],
     answerIndex: 0,
     id: "ku3ncafjt6afyjf15n",
   },
-  {
+  uir7fxwgxj1k4j2i3i: {
     subject: "geography",
     question: "What is the world's largest desert? ",
     options: ["Sahara Desert", "Gobi Desert", "Kalahari Desert"],
     answerIndex: 0,
     id: "uir7fxwgxj1k4j2i3i",
   },
-  {
+  duc8h2ywmap5eysghn: {
     subject: "geography",
     question: "Which African country is the most populous? ",
     options: ["Nigeria", "Egypt", "Ethiopia"],
     answerIndex: 0,
     id: "duc8h2ywmap5eysghn",
   },
-  {
+  iiclv88q8hmt79hu8m: {
     subject: "geography",
     question: "Which river forms the border between India and Bangladesh? ",
     options: ["Ganges River", "Indus River", "Brahmaputra River"],
     answerIndex: 2,
     id: "iiclv88q8hmt79hu8m",
   },
-  {
+  ptslz3aoelrsotypec: {
     subject: "geography",
     question:
       "What country is both the smallest by land area and has the smallest population?",
@@ -966,25 +966,25 @@ module.exports = [
     answerIndex: 0,
     id: "ptslz3aoelrsotypec",
   },
-  {
+  dh7947pn5qacmkwl89: {
     subject: "geography",
     question: "What is the capital of Thailand?",
     options: ["Bangkok", "Phuket", "Chiang Mai"],
     answerIndex: 0,
     id: "dh7947pn5qacmkwl89",
   },
-  {
+  fcqcg4jxcz9ti3ojpt: {
     subject: "geography",
     question: "What is the tallest mountain in North America?",
     options: ["Denali", "Mount St. Helens", "Mount Rainier"],
     answerIndex: 0,
     id: "fcqcg4jxcz9ti3ojpt",
   },
-  {
+  c92n0blngg0kb0omg1: {
     subject: "geography",
     question: "What country makes up the eastern border of Tunisia?",
     options: ["Algeria", "Libya", "Egypt"],
     answerIndex: 0,
     id: "c92n0blngg0kb0omg1",
   },
-];
+};
--- api/routes/questions.js
+++ api/routes/questions.js
@@ -5,6 +5,11 @@ var router = express.Router();
 
 // const questionsWithIds = questions.map((question, index) => ({...question, id: question.id+ Math.random().toString(36).substring(2, 11)}));
 
+// const newQuestions = questions.reduce((acc, question) => {
+//   acc[question.id] = question;
+//   return acc;
+// }, {});
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.json(questions);
