12ab4ad40d2bec9890ac6b5f97dfef962b340ae7
Jeremy Zevin <jzevin@nbme.org>
2022-09-13 10:33:28 -0400

    add ids

--- api/routes/questions.js
+++ api/routes/questions.js
@@ -3,11 +3,11 @@ var express = require('express');
 var questions = require('../models/questions');
 var router = express.Router();
 
-const questionsWithIds = questions.map((question, index) => ({...question, id: index}));
+// const questionsWithIds = questions.map((question, index) => ({...question, id: question.id+ Math.random().toString(36).substring(2, 11)}));
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.json(questionsWithIds);
+  res.json(questions);
 });
 
 module.exports = router;
