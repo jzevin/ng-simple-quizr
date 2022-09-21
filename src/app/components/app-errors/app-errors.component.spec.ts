efaa130c42169489f56f7a19323f816dd639fbe4
Jeremy Zevin <jzevin@nbme.org>
2022-09-21 12:45:29 -0400

    improve app-errors unit testing

--- src/app/components/app-errors/app-errors.component.spec.ts
+++ src/app/components/app-errors/app-errors.component.spec.ts
@@ -1,35 +1,41 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
 
 import { AppErrorsComponent } from './app-errors.component';
 import { DebugElement } from '@angular/core';
-import { provideMockStore } from '@ngrx/store/testing';
+import { MemoizedSelector } from '@ngrx/store';
+import { QuizState } from 'src/app/models/quiz.models';
+import { selectErrors } from '../../state/quiz.state.selectors';
 
 describe('AppErrorsComponent', () => {
   let component: AppErrorsComponent;
   let fixture: ComponentFixture<AppErrorsComponent>;
   let elDebug: DebugElement;
   let el: HTMLElement;
+  let mockSelectErrors: MemoizedSelector<QuizState, Error[]>;
+  let mockStore: MockStore;
 
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [AppErrorsComponent],
-      providers: [provideMockStore(
-        {
-          initialState: {
-            quiz: {
-              errors: [new Error('test error 1'), new Error('test error 2')]
-            }
-          }
-        }
-      )],
+      providers: [provideMockStore()],
     }).compileComponents();
 
+    mockStore = TestBed.inject(MockStore);
+    mockSelectErrors = mockStore.overrideSelector(
+      selectErrors,
+      [new Error('test error 1'), new Error('test error 2')]
+    );
     fixture = TestBed.createComponent(AppErrorsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
     el = fixture.nativeElement as HTMLElement;
     elDebug = fixture.debugElement;
+  });
+
+  afterEach(() => {
+    mockStore.resetSelectors();
   });
 
   it('should create', () => {
@@ -58,6 +64,27 @@ describe('AppErrorsComponent', () => {
 
   it('should have an error message with the text "test error 2"', () => {
     expect(el.querySelectorAll('.error .message')[1]?.textContent).toBe('test error 2');
+  });
+
+  it('should call onClickClose method with the error index when .error elements are clicked', () => {
+    const compSpy = spyOn(component, 'onClickClose');
+    const errorBtnElements = el.querySelectorAll('.error button');
+    errorBtnElements.forEach((el, errorIndex) => {
+      (el as HTMLLIElement).click();
+      expect(compSpy).toHaveBeenCalledWith(errorIndex);
+    });
+    fixture.detectChanges();
+    expect(compSpy).toHaveBeenCalledTimes(2);
+  });
+
+  it('should dispatch "[User] Close error" when clicked', () => {
+    const errorBtnElements = el.querySelectorAll('.error button');
+    const e1 = errorBtnElements[1] as HTMLLIElement;
+    e1.click();
+    fixture.detectChanges();
+    mockStore.scannedActions$.subscribe(action => {
+      expect(action.type).toBe('[User] Close error');
+    }); // NOTE: No need to unsubscribe since the ref is getting wiped out before each
   });
 
 });
