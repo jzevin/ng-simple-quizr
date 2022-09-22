327beb326c06c19774dbe8ded13558ce2c09fd0f
Jeremy Zevin <jzevin@nbme.org>
2022-09-22 18:35:11 -0400

    add api service tets coverage

--- src/app/services/api.service.spec.ts
+++ src/app/services/api.service.spec.ts
@@ -1,19 +1,47 @@
 import { ApiService } from './api.service';
-import { HttpClientModule } from '@angular/common/http';
-import { TestBed } from '@angular/core/testing';
+import { HttpClient } from '@angular/common/http';
+import { QuizQuestion } from '../models/quiz.models';
+import { QuizStateTestFixture } from '../testing/quiz.state.fixtures';
+import { of } from 'rxjs';
 
 describe('ApiService', () => {
-  let service: ApiService;
+  let apiService: ApiService;
+  let httpClientSpy: jasmine.SpyObj<HttpClient>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [HttpClientModule]
-    })
-;
-    service = TestBed.inject(ApiService);
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+    apiService = new ApiService(httpClientSpy);
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(apiService).toBeTruthy();
+  });
+
+  it('should have a getQuizQuestions method', () => {
+    expect(apiService.getQuizQuestions).toBeTruthy();
+  });
+
+  it('should have a getQuizQuestions method', () => {
+    const serviceSpy = spyOn(apiService, 'getQuizQuestions');
+    apiService.getQuizQuestions();
+    expect(serviceSpy).toHaveBeenCalled();
+  });
+
+  it('should return expected heroes (HttpClient called once)', (done: DoneFn) => {
+    const expectedQuestion: QuizQuestion =
+      QuizStateTestFixture.questions[1];
+
+    httpClientSpy.get.and.returnValue(of(QuizStateTestFixture.questions));
+
+    apiService.getQuizQuestions().subscribe({
+      next: (questions) => {
+        expect((questions as readonly QuizQuestion[])[1])
+          .withContext('expected questions')
+          .toEqual(expectedQuestion);
+        done();
+      },
+      error: done.fail,
+    });
+    expect(httpClientSpy.get.calls.count()).withContext('one call').toBe(1);
   });
 });
